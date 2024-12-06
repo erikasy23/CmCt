a531d0a887f006e99d5b8950055c068ca48eab6e
basnetritu <ritu.basnet@nasa.gov>
2024-12-06 21:09:45 +0000

    Deleted check_datarange function

--- bin/Gravimetry/gravimetry_utils.py
+++ bin/Gravimetry/gravimetry_utils.py
@@ -48,24 +48,6 @@ def set_projection(loc):
 
 
 
-
-### Check the selected dates are within the range of model data
-def check_datarange(time_var,start_date_cftime, end_date_cftime):
-
-    calendar_type = time_var.to_index().calendar
-       
-    # Get the minimum and maximum values directly from the time variable
-    min_time = time_var.values.min()
-    max_time = time_var.values.max()
-    
-    # Check if the selected start and end dates are within the range
-    if min_time <= start_date_cftime <= max_time and min_time <= end_date_cftime <= max_time:
-        print(f"The selected dates {start_date_cftime} and {end_date_cftime} are within the range of the model data.")
-    else:
-        raise ValueError(f"Error: The selected dates {end_date_cftime} or {end_date_cftime} are out of range. Model data time range is from {min_time} to {max_time}.")
-
-
-
 ### Check the selected dates are within the range of model data
 def check_datarange(time_var,start_date_cftime, end_date_cftime):
 
